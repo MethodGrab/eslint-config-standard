b6477b27278b1044120999150e5ac6df65572907
MethodGrab <methodgr4b@gmail.com>
2015-05-26 09:02:25 +0100

    Style clean up

--- strict.js
+++ strict.js
@@ -8,11 +8,18 @@ var merge = require( 'lodash/object/merge' );
 var extras = {
 
 	'rules' : {
+		// Best practices
 		'no-param-reassign'   : 2,
+
+		// Strict mode
 		'strict'              : [ 2, 'global' ],
+
+		// Node.js
 		'handle-callback-err' : [ 2, '^(err|error)$' ],
 		'no-mixed-requires'   : [ 1, true ],
 		'no-sync'             : 1,
+
+		// Stylistic issues
 		'new-cap'             : 1,
 		'new-parens'          : 1,
 		'no-lonely-if'        : 1,
